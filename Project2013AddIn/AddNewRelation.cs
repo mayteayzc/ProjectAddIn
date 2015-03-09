cae461d296dbbd74c9a574f870fff310d12f3a62
mayteayzc <mayteayzc@gmail>
2015-03-09 18:08:09 +0800

    messagebox, check input

--- Project2013AddIn/AddNewRelation.cs
+++ Project2013AddIn/AddNewRelation.cs
@@ -15,10 +15,11 @@ namespace Project2013AddIn
 {
     public partial class AddNewRelation : Form
     {
+        MSProject.Project project = Globals.ThisAddIn.Application.ActiveProject;
+
         public AddNewRelation()
         {
             InitializeComponent();
-            MSProject.Project project = Globals.ThisAddIn.Application.ActiveProject;
             int count = project.Tasks.Count;
             int index = 0;
             string[] datasource = new string[count];
@@ -30,7 +31,7 @@ namespace Project2013AddIn
                 datasource[index++] = name;
             }
             this.ComboBoxAct1.DataSource = datasource;
-            this.ComboBoxAct2.DataSource = datasource;
+            this.ComboBoxAct2.DataSource = datasource.Clone();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
@@ -40,10 +41,56 @@ namespace Project2013AddIn
 
         private void btnOK_Click(object sender, EventArgs e)
         {
-            if(this.ComboBoxAct1.SelectedIndex == this.ComboBoxAct2.SelectedIndex) {
-                MessageBox.Show("Please re-enter");
+            if(this.ComboBoxAct1.SelectedItem==null||this.ComboBoxAct2.SelectedItem==null||ComboBoxRela.SelectedItem==null)
+            {
+                DialogResult result=MessageBox.Show("Please fill in all feilds.");
             }
-            this.Hide();
+            
+            else if(this.ComboBoxAct1.SelectedIndex == this.ComboBoxAct2.SelectedIndex)
+            {
+                MessageBox.Show("Please select two different activities.");
+            }
+            else
+            {
+                int DurAct1=0,DurAct2=0;
+                DateTime StartAct1,StartAct2,EndAct1,EndAct2; 
+                bool found1=false,found2=false;
+
+                foreach (MSProject.Task task in project.Tasks)
+                {
+                    if (task.Name == this.ComboBoxAct1.SelectedItem.ToString())
+                    {
+                        StartAct1 = task.ScheduledStart;
+                        EndAct1 = task.ScheduledFinish;
+                        DurAct1 = task.ScheduledDuration;
+                        found1=true;
+                    }
+
+                    if (task.Name == this.ComboBoxAct2.SelectedItem.ToString())
+                    {
+                        StartAct2 = task.ScheduledStart;
+                        EndAct2 = task.ScheduledFinish;
+                        DurAct2 = task.ScheduledDuration;
+                        found2=true;
+                    }
+                }
+                
+                if (found1==false||found2==false)
+                {
+                    MessageBox.Show("Error: Task can not be found.");
+                    this.Activate();
+                }
+
+                if (this.ComboBoxRela.SelectedItem.ToString().Equals("Concurrent"))
+                {
+                    int MaxDur = Math.Max(DurAct1, DurAct2);
+                    DurAct1 = MaxDur;
+                    DurAct2 = MaxDur;
+
+
+                }
+                this.Hide();
+            }
         }
 
         private void AddNewRelation_Load(object sender, EventArgs e)
